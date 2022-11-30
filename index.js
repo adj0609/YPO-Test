ed5c8793faad779ffaef4b178ad7868f9ef5302b
Andrea Jared <adj0803@gmail.com>
2022-11-29 21:30:23 -0500

    finished

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-let button = document.querySelector("#button2");//select your button
-button.addEventListener('click',function(){//handle click event
-document.querySelector("#box1").style.backgroundColor="Blue";//select box id and set background
-});
+
+
+
+
+
+
+
+
+
 
 
 
